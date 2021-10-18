9ec4b22066975c43a8c9e6984b7839e05d1cb815
InvalitSyntax <81087114+InvalitSyntax@users.noreply.github.com>
2021-10-18 20:12:45 +0700

    Add files via upload

--- flags.sh
+++ flags.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 #pip install pyzbar
 cd ~/catkin_ws/src/clover/
-git clean -df
-git checkout -- .
-git remote add InvalitSyntax https://github.com/InvalitSyntax/clover.git
+git remote add timkondratiev https://github.com/InvalitSyntax/clover.git
